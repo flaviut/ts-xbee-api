a9f2a28aadddc5cb8fcbdc1fdd45cd951627073b
Flaviu Tamas <me@flaviutamas.com>
2023-06-25 00:49:56 -0400

    Fix frame id generation

--- src/lib/frame-builder.ts
+++ src/lib/frame-builder.ts
@@ -49,7 +49,7 @@ function FrameBuilder() {
     },
 
     getFrameId: function getFrameId(frame: { id?: Uint8 }): Uint8 {
-      frame.id = frame.id && frame.id !== 0 ? frame.id : this.nextFrameId();
+      frame.id = frame.id != null || frame.id === 0 ? frame.id : this.nextFrameId();
       return frame.id;
     },
 
